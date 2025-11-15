64c710a4bfbea6e5eb8f4dce88b60f6649e05b0f
Eduardo Fabian Chacaliaza Minaya <echacaliazaminaya@gmail.com>
2025-11-14 23:58:03 -0500

    chore: initialize Spring Boot project (Java 25)

---
+++ src/main/java/com/mindora/platform/MinDoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindora.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinDoraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinDoraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mindora/platform/MinDoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindora.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinDoraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
